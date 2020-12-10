10d0a01661c7ca1eaf829f02d4ae7a07a3d858da
kserranoa <kserranoa16@gmail.com>
2020-12-09 23:50:15 -0600

    inicializacion

---
+++ TallerThreadsCcc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TallerThreadsCcc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
